b41583bfdf1d56f13986a4cae1e8f88076e0c227
emrehansmsk194 <emrehansimsek194@gmail.com>
2024-04-07 17:30:06 +0300

    Completed FileLogger, LogHandler and ReservationRepository classes.

--- FileLogger.cs
+++ FileLogger.cs
@@ -4,7 +4,13 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 public class FileLogger : ILogger{
-    public void Log(LogRecord log){
+    private readonly string _logFilepath; // readonly => yalnizca constructorda deger atanabilir
 
+    public FileLogger(string logFilepath){
+        _logFilepath = logFilepath;
+    }
+    public void Log(LogRecord log){
+        var logJson = JsonSerializer.Serialize(log, new JsonSerializerOptions{WriteIndented = true});
+        File.AppendAllText(_logFilepath, logJson + Environment.NewLine);
     }
 }
--- ReservationRepository.cs
+++ ReservationRepository.cs
@@ -6,17 +6,34 @@ using System.Text.Json.Serialization;
 class ReservationRepository: IReservationRepository{
 
     public List <Reservation> _reservations = new List<Reservation>();
+    private readonly string _reservationDataFilePath;
+    private readonly ILogger _logger;
+
+    public ReservationRepository(string reservationDataFilePath, ILogger logger)
+    {
+        _reservationDataFilePath = reservationDataFilePath;
+        _logger = logger;
+    }
 
     public void AddReservation(Reservation reservation){
         _reservations.Add(reservation);
-        // Update ReservationData.json
+        UpdateReservationDataFile();
+        _logger.Log(new LogRecord(DateTime.Now,reservation.reserverName, $"Added Reservation: {reservation.room.RoomName}"));
+
+
     }
     public void DeleteReservation(Reservation reservation){
         _reservations.Remove(reservation);
+        UpdateReservationDataFile();
+        _logger.Log(new LogRecord(DateTime.Now,reservation.reserverName, $"Deleted Reservation: {reservation.room.RoomName}"));
     }
     public List<Reservation> GetAllReservations(){
         return _reservations;
     }
     
+    public void UpdateReservationDataFile(){
+        var reservationJson = JsonSerializer.Serialize(_reservations, new JsonSerializerOptions {WriteIndented = true});
+        File.WriteAllText(_reservationDataFilePath,reservationJson);    
+    }
 
 }
